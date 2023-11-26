2591a4139a9fe53f960bf85ed7b8d389de2763d5
Mfoniso Emem <mfonisoemem@gmail.com>
2023-11-26 10:32:53 +0100

    Creating a portolio website

---
+++ src/Components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './navbar.css';
+
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+    <div className='desktopmenu'>
+
+    </div>
+    <div className='aboutme'></div>
+
+    </nav>
+   
+  )
+}
+
+export default Navbar
